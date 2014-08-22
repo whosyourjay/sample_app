075ee5ee10ab71a86c5aae1c58336e3e62a3e8dc
Joseph Swernofsky <Joseph.Swernofsky@gmail.com>
2014-08-21 17:31:30 -0700

    Added contact page, simplified StaticPages controller spec (exercise 3-1, 3-2)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+	describe "Home page" do
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -19,7 +21,7 @@ describe "StaticPages" do
 		end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 	end
 
@@ -31,7 +33,14 @@ describe "StaticPages" do
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
+
+	describe "Contact page" do
+		it "should have title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+		end
+	end
 end
